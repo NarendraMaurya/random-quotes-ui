aeb4a4de9823f0844d6369fd3151b5f6a6de3784
NarendraMaurya <92203427+NarendraMaurya@users.noreply.github.com>
2023-07-06 21:29:00 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './footer'
 import ShowQuotes from './ShowQuotes'
 import Img from './image/bg-img.jpg'
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div style={{backgroundImage:`url(${Img})`,backgroundSize:'cover',height:'100vh',width:'100vw'}}>
             <Navbar/>
             <ShowQuotes/>
+            <Footer />
         </div>
     )
 }
