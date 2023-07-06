ea42fe16c0037d686c4d94364f085f850a2b2d3e
NarendraMaurya <92203427+NarendraMaurya@users.noreply.github.com>
2023-07-06 21:27:28 +0530

    footer added

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  './footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <p>Made with <span>&#10084;</span> Narendra Maurya</p>
+    </footer>
+  )
+}
+
+export default Footer;
